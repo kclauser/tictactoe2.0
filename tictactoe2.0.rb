d125f5657a65b743ee328c1a3f2fba36592199f6
Chad Oakley <Chad.Oakley@tampabay.rr.com>
2015-09-20 18:28:34 -0400

    final

--- tictactoe2.0.rb
+++ tictactoe2.0.rb
@@ -1,31 +1,36 @@
 require "sinatra"
 require "json"
+require 'sinatra/reloader' if development?
 
 class Board
   attr_accessor :positions
-
-  @winning_combos = [1,2,3],
-                    [4,5,6],
-                    [7,8,9],
-                    [1,4,7],
-                    [2,5,8],
-                    [3,6,9],
-                    [3,5,7],
-                    [1,5,9]
-  @count = 0
-
   def initialize
     self.positions = [" ", " ", " ",
                       " ", " ", " ",
                       " ", " ", " "]
+    @count = 0
+  end
+
+  def valid_move(position)
+    self.positions[position] != "X" && self.positions[position] != "O"
   end
 
   def move(player, position)
     self.positions[position] = player
+    @count += 1
   end
 
   def status(player)
-    if @winning_combos.any?{ |win|
+    winning_combos = [[1,2,3],
+                      [4,5,6],
+                      [7,8,9],
+                      [1,4,7],
+                      [2,5,8],
+                      [3,6,9],
+                      [3,5,7],
+                      [1,5,9]]
+
+    if winning_combos.any?{ |win|
     win.all? { |position| self.positions[position] == player}}
     "Player Wins"
     elsif @count == 9
@@ -67,17 +72,18 @@ post "/game" do
 end
 
 post "/move" do
-  player = params["player"]
+  player = params["player"].upcase
   position = params["position"].to_i
 
   board = settings.board
 
-  board.move(player, position)
-# NOTE if/else to test if valid or else invalid 409
+  if board.valid_move(position) == false
+
   response = {
-    "status" => "ok",
+    "status" => "invalid",
+    "reason" => "player already moved there",
     "board" => {
-      "state" => board.status(player,position),
+      "state" => board.status(player),
       "position" => {
         "0": board.positions[0],
         "1": board.positions[1],
@@ -87,10 +93,33 @@ post "/move" do
         "5": board.positions[5],
         "6": board.positions[6],
         "7": board.positions[7],
-        "8": board.positions[8],
+        "8": board.positions[8]
+      }
+    }
+  }
+  status 409
+  response.to_json
+
+  else
+  board.move(player,position)
+  response = {
+    "status" => "ok",
+    "board" => {
+      "state" => board.status(player),
+      "position" => {
+        "0": board.positions[0],
+        "1": board.positions[1],
+        "2": board.positions[2],
+        "3": board.positions[3],
+        "4": board.positions[4],
+        "5": board.positions[5],
+        "6": board.positions[6],
+        "7": board.positions[7],
+        "8": board.positions[8]
       }
     }
   }
 
   response.to_json
+  end
 end
